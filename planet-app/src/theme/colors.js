80998ff67e8c3f4749c73193d5ce7f3774392586
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2021-10-02 03:46:06 +0600

    Add some colors

---
+++ planet-app/src/theme/colors.js
@@ -0,0 +1,15 @@
+export const color = {
+    black: '#070724',
+    white: '#fff',
+    darkGray: '#38384F',
+    gray: '#838391',
+    lightGreen: '#419EBB',
+    orange: '#EDA249',
+    purple: '#6f2ed6',
+    darkOrange: '#D14C32',
+    red: '#D83A34',
+    lightRed: '#CD5120',
+    green: '#1ec2a4',
+    blue: '#2d68f0',
+
+}
